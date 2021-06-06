0402780985389a1c6bf89bf6afe9e456c3756806
KrishnaPoul105 <71781923+KrishnaPoul105@users.noreply.github.com>
2021-06-06 19:59:50 +0530

    feeds routes created

--- Server/routes/feedsRoute.js
+++ Server/routes/feedsRoute.js
@@ -1,9 +1,8 @@
 var multer = require("multer");
 const express = require("express");
 const mongoose = require("mongoose");
-
-//const Folders = mongoose.model("Folder");
-//const Posts = mongoose.model("Post");
+const Folders = mongoose.model("Folder");
+const Posts = mongoose.model("Post");
 
 const requireToken = require("../middleware/requireToken");
 
@@ -31,4 +30,35 @@ app.post("/upload", upload.single("file"), ({ body, file }, res) => {
     },
   });
 });
+
 //addfolder,deletefolder,addpost,deletepost
+
+router.get("/addfolder", async (req, res) => {
+    const folder = new Folders(req.body);
+
+  try {
+    await folder.save();
+    res.send("Folder Created")
+    console.log("Folder Created");
+  } catch (error) {
+    console.log("Folder not Created");
+  }  
+});
+router.get("/deletefolder", async (req, res) => {
+    const post = await Folders.deleteOne({ userid: req.user._id }); 
+});
+router.get("/addpost", async (req, res) => {
+    const post = new Posts(req.body);
+
+  try {
+    await post.save();
+    res.send("Post Created")
+    console.log("Post Created");
+  } catch (error) {
+    console.log("Post not Created");
+  }  
+});
+router.get("/deletepost", async (req, res) => {
+    const post= await Folders.deleteOne({ folderid: req.user._id }); 
+});
+module.exports = router;
