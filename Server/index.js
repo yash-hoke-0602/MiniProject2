9b080f6c74f0beea78f5730d77a8fc5191e232ae
yash-hoke-0602 <yashhoke0206@gmail.com>
2021-06-21 08:39:37 -0700

    hosting commit

--- Server/index.js
+++ Server/index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const { mogoUrl } = require("./keys");
 
 require("./models/User");
